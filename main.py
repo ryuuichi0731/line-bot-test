b5e6aaf1dec4d01112bd52d6ef5ba1128731fcb0
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-03-07 18:20:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 [
-                    TextSendMessage(text='メッセージを送信しました。一つしたにあるメッセージをタップするか、トーク画面下にある選択ボタンからお選びください。'),
+                    TextSendMessage(text='メッセージを送信しました。一つしたにあるメッセージをタップするか、トーク画面下にある選択ボタンからお選びください。')],
                     TemplateSendMessage({
                       "type": "template",
                       "altText": "this is a carousel template",
@@ -106,48 +106,20 @@ def handle_message(event):
                               }
                             ]
                           }
-                        ]
                       }
                     }
                     ),
-                    QuickReplyButton({
-                      "type": "text",
-                      "text": "選択肢の中から現在利用している施設を選んでダブルタップしてください",
-                      "quickReply": {
-                        "items": [
-                          {
-                            "type": "action",
-                            "imageUrl": "https://d.kuku.lu/ce8010821a",
-                            "action": {
-                              "type": "message",
-                              "label": "ショッピングセンター",
-                              "text": "shopping center"
-                            }
-                          },
-                          {
-                            "type": "action",
-                            "imageUrl": "https://d.kuku.lu/3715b32de3",
-                            "action": {
-                              "type": "message",
-                              "label": "けいはんなオープンイノベーションセンター",
-                              "text": "open innovation centre"
-                            }
-                          },
-                          {
-                            "type": "action",
-                            "imageUrl": "https://d.kuku.lu/5d0b75f5e6",
-                            "action": {
-                              "type": "message",
-                              "label": "ライトハウス情報文化センター",
-                              "text": "media and communications center"
-                            }
-                          }
-                        ]
-                      }
-                    }
-                    )
-                ] 
-            ),
+            quick_reply=QuickReply(
+                        items=[
+                        QuickReplyButton(
+                            action=PostbackAction(label="ショッピングセンター", text="ショッピングセンター")
+                        ),
+                        QuickReplyButton(
+                        action=PostbackAction(label="けいはんなオープンイノベーションセンター", text="けいはんなオープンイノベーションセンター")
+                        ),
+                        QuickReplyButton(
+                            action=PostbackAction(label="情報文化センター", text="情報文化センター")
+                        )
     
   
     
