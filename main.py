5e84a61532b431b1ecce0ecb890034408acc8f6c
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-03-19 00:17:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -72,12 +72,12 @@ def handle_message(event):
                                     action=MessageAction(label="情報文化センター", text="Media & Communication center")
                         ),
                     ])))
-    else:
-        line_bot_api.reply_message( 
-            event.reply_token,    
-            TextSendMessage(        
-                text='返答内容を理解することができませんでした。大変申し訳ありませんが、別の回答をお試しください。')
-        )
+        else:
+            line_bot_api.reply_message( 
+                event.reply_token,    
+                TextSendMessage(        
+                    text='返答内容を理解することができませんでした。大変申し訳ありませんが、別の回答をお試しください。')
+            )
             
 
     if event.type == "message":
@@ -135,12 +135,12 @@ def handle_message(event):
                                     action=MessageAction(label="会議室", text="Conference Room")
                         ),
                     ])))
-    else:
-        line_bot_api.reply_message( 
-            event.reply_token,    
-            TextSendMessage(        
-                text='返答内容を理解することができませんでした。大変申し訳ありませんが、別の回答をお試しください。')
-        )
+        else:
+            line_bot_api.reply_message( 
+                event.reply_token,    
+                TextSendMessage(        
+                    text='返答内容を理解することができませんでした。大変申し訳ありませんが、別の回答をお試しください。')
+            )
     
 
     if event.type == "message":
@@ -170,12 +170,12 @@ def handle_message(event):
                 event.reply_token,
                 TextSendMessage(text='案内をキャンセルしました。')
             )
-    else:
-        line_bot_api.reply_message( 
-            event.reply_token,    
-            TextSendMessage(        
-                text='返答内容を理解することができませんでした。大変申し訳ありませんが、別の回答をお試しください。')
-        )
+        else:
+            line_bot_api.reply_message( 
+                event.reply_token,    
+                TextSendMessage(        
+                    text='返答内容を理解することができませんでした。大変申し訳ありませんが、別の回答をお試しください。')
+            )
                       
                 
     message_content = line_bot_api.get_message_content(event.message.id)
