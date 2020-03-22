b84677d2a00e6bbe7ff716bd9070eeb88cd2f5f5
ryuuichi0731 <39109695+ryuuichi0731@users.noreply.github.com>
2020-03-22 11:25:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def handle_message(event):
                         quick_reply=QuickReply(
                             items=[
                                 QuickReplyButton(
-                                    action=MessageAction(label="ワイルドなスパイス", text="Shopping center")
+                                    action=MessageAction(label="ショッピングセンター", text="Shopping center")
                         ),
                                 QuickReplyButton(
                                     action=MessageAction(label="科学技術センター", text="Science & Technology center")
